c00794584045eba16e3a2d440d0e870aea9014bd
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-16 16:55:02 +0530

    Added cpp templates for different contests

--- template/template_test_case_number_output.cpp
+++ template/template_test_case_number_output.cpp
@@ -20,9 +20,9 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #ifdef INPUT
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+    #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
-#define dbg(...)
+    #define dbg(...)
 #endif
 
 void solve() {
@@ -34,27 +34,26 @@ int main(){
     cin.tie(0);
     cout.tie(0);
     cout << fixed;
+
     #ifdef INPUT  
-        freopen("a.in","r",stdin);
+        freopen("PROBLEM_NAME.in","r",stdin);
         clock_t time_req; 
         cout << endl;
-        
     #endif 
 
-    int testCase;
-    cin >> testCase;
-	
-    while(testCase--){
+    int totalTestCases;
+    cin >> totalTestCases;
+    for (int testCase = 1; testCase <= totalTestCases; testCase++) {
+        cout << "Case #" << testCase << ": ";
         solve();
-        cout << endl;
     }
 
-    #ifdef INPUT
-        
+    #ifdef INPUT    
         cout << endl << endl;
         time_req = clock();
         cout << "Processor time taken : " << (float)time_req/CLOCKS_PER_SEC << " seconds" << endl << endl; 
     #endif
+    
     return 0;
 }
 
