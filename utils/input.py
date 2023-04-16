b34e197189d92119e4643860a523b40421b7c93b
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-16 16:56:15 +0530

    Added Input and generator utility functions

---
+++ utils/input.py
@@ -0,0 +1,13 @@
+class InputUtils: 
+    def menu(inputFields, property: str) -> str: 
+        print(f"Choose {property}")
+        for index, inputField in enumerate(inputFields):
+            print(f"{index + 1}: {inputField}")
+
+        try: 
+            return inputFields[int(input(f"Choice: ")) - 1]
+        except:
+            retry_input = input("Invalid input, retry? (y/n): ")
+            if retry_input == "Y" or retry_input == "y": input_menu(inputFields, property)
+            else: exit(0)
+    
---
+++ utils/utils.py
@@ -0,0 +1,11 @@
+def codechef_contest_generator(number_of_problems: int):
+    print(f"Generating {number_of_problems} problem")
+
+def codeforces_contest_generator(number_of_problems: int):
+    pass 
+
+def google_hash_code_contest_generator(number_of_problems: int):
+    pass
+
+def google_code_jam_contest_generator(number_of_problems: int): 
+    pass
