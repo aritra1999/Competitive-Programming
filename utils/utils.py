1bb05747a47d669e4142e170494e15388ab781e5
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-19 10:35:55 +0530

    Added `create_dir_structure` to build dir structure for given platform and contest

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,22 @@
 import os
 
+
+
 def generate_contest(contest_code, number_of_problems, contest_meta, platform):
-    BASE_PATH = f"{platform}/{contest_code}"
-    os.mkdir(BASE_PATH),
+    create_dir_structure(platform, contest_code)
+
+
+def create_dir_structure(platform, contest_code):
+
+    contest_dir = f"{platform}/{contest_code}"
+
+    try: 
+        check_and_create_dir(platform)
+        check_and_create_dir(contest_dir)
+    except: 
+        print("Error creating dir")
+
+
+def check_and_create_dir(dir):    
+    if not os.path.exists(dir):
+        os.mkdir(dir)
